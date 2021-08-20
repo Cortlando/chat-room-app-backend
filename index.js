1406eaeeb5453969ac6d0ea3b560c7093050ad4a
Cortlando <48654220+Cortlando@users.noreply.github.com>
2021-08-20 14:15:01 -0500

    Changed the way i'm instantiating socket.io

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const app = express()
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+// const express = require('express')
+// const app = express()
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+
+const express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server)
+
+server.listen(process.env.PORT || 3000);
 
 const corsOptions={
     cors: true,
     origins:["http://localhost:3000"],
    }
    
-const io = new Server(server, corsOptions);
+//const io = new Server(server, corsOptions);
 
-const port = process.env.PORT || "4000"
+//const port = process.env.PORT || "4000"
 
 function getRandomInt() {
     return Math.floor(Math.random() * 1000);
