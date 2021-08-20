192fdfee076ae3c3b31e81e14e4851de8b5b9c0d
Cortlando <48654220+Cortlando@users.noreply.github.com>
2021-08-20 14:00:22 -0500

    changed port number

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions={
    
 const io = new Server(server, corsOptions);
 
-const port = 4000
+const port = process.env.PORT || "4000"
 
 function getRandomInt() {
     return Math.floor(Math.random() * 1000);
